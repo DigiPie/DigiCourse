d8b60d0275d99aca5dbe0cd8ce37d9aca73f0262
Lee Tze Ting <e0015031@u.nus.edu>
2019-10-13 16:38:14 +0800

    Add checks for enrollment such that prof cannot accept students > capacity
    - TA can be accepted irregardless of the capacity

--- src/routes/enrollments.js
+++ src/routes/enrollments.js
@@ -7,21 +7,33 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
 
+var current_capacity;
+var increase_count;
 
 /**** Routing ****/
 router.get('/', function(req, res, next) {
+    const capacity_query = `SELECT c_capacity FROM Courses WHERE c_id = '${req.params.cid}'`;
+
     var sql_query = `SELECT s_id, c_id, req_type, p_id, req_status, TO_CHAR(req_datetime, 'Dy Mon DD YYYY HH24:MI:SS') req_datetime 
     FROM Enrollments WHERE c_id =\'${req.params.cid}\' AND NOT req_status`;
 
-	pool.query(sql_query, (err, data) => {
-        res.render('enrollments', {
-            isCourse: req.isCourse, 
-            username: req.username,
-            accountType: req.accountType, 
-            cid: req.cid,
-            data: req.data,
-            datarows: data.rows
-        });
+    pool.query(capacity_query, (err, cdata) => {
+        current_capacity = cdata.rows[0].c_capacity;
+        var new_capacity = increase_count == undefined ? current_capacity + 10 : increase_count; 
+        increase_count = undefined;
+
+        pool.query(sql_query, (err, data) => {
+            res.render('enrollments', {
+                isCourse: req.isCourse, 
+                username: req.username,
+                accountType: req.accountType, 
+                cid: req.cid,
+                data: req.data,
+                datarows: data.rows,
+                capacity: current_capacity,
+                new_capacity: new_capacity 
+            });
+        });  
     });
     
 });
@@ -62,36 +74,93 @@ router.post('/accept', function(req, res, next) {
     }
 
     var sids = [];
+    var ta_rows = [];
+    var t_sid = [];
+    var s_sid = [];
+    
     for(var i = 0; i < selected_rows.length; i++) {
         delete selected_rows[i].accepted;
         selected_rows[i].req_status = true;
         selected_rows[i].p_id = account_uid;
         sids.push(selected_rows[i].s_id);
+
+        if (selected_rows[i].req_type == 0) {
+            ta_rows.push(selected_rows[i]);
+            t_sid.push(selected_rows[i].s_id);
+        } else {
+            s_sid.push(selected_rows[i].s_id);
+        }
     }
     
-    sids = sids.join(', ');
+    const scount = s_sid.length;
 
-    if (selected_rows.length == 0) {
-        req.flash('error', `Please select a student`);
-        res.status(400).redirect('back');
-    }
+    sids = sids.join(', ');
+    t_sid = t_sid.join(', ');
+    s_sid = s_sid.join(', ');
 
     const column_set = new pgp.helpers.ColumnSet(['?s_id','?c_id', '?req_type', '?req_datetime', 'req_status', 'p_id'], {table: 'enrollments'});
-    const update_sql = pgp.helpers.update(selected_rows, column_set) 
-    + ' WHERE v.s_id = t.s_id AND v.c_id = t.c_id AND TO_CHAR(t.req_datetime, \'Dy Mon DD YYYY HH24:MI:SS\') = v.req_datetime';
-    
-	pool.query(update_sql, (err, data) => {
-        if (err) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: "Something went wrong during the update, try again later.",
-                error: err
-            });
+    const where_sql = ' WHERE v.s_id = t.s_id AND v.c_id = t.c_id AND TO_CHAR(t.req_datetime, \'Dy Mon DD YYYY HH24:MI:SS\') = v.req_datetime';
+    var check_query = `SELECT c_capacity - (
+            SELECT COUNT(*) scount
+            FROM CourseEnrollments
+            WHERE c_id = '${req.params.cid}'
+            AND req_type = 1) available
+        FROM Courses
+        WHERE c_id = '${req.params.cid}'`;
+
+	pool.query(check_query, (err, data) => {
+        if (data.rows[0].available < scount) {
+            increase_count = scount - data.rows[0].available + current_capacity;
+            console.log('1');
+            if (ta_rows.length != 0) {
+                console.log('2a 1');
+                const update_sql = pgp.helpers.update(ta_rows, column_set) + where_sql;
+                pool.query(update_sql, (err, data) => {
+                    console.log('3');
+                    if (err) {
+                        console.log('4');
+                        res.status(err.status || 500);
+                        res.render('error', {
+                            message: "Something went wrong during the update, try again later.",
+                            error: err
+                        });
+                    } else {
+                        console.log('5');
+                        req.flash('success', `Successfully enrolled ${t_sid} as Teaching Assistant.\n `);
+
+                        if (s_sid.length != '') {
+                            req.flash('error', `Course capacity have reached, please increase the capacity to ${increase_count} to enroll ${s_sid}.`);
+                        }
+                        res.status(200).redirect('back');
+                        return;
+                    }
+                });
+
+            } else {
+                console.log('2a 2');
+
+                req.flash('error', `Course capacity have reached, please increase the capacity to ${increase_count} to enroll ${s_sid}.`);
+                res.status(400).redirect('back');
+                return;
+            }
         } else {
-            req.flash('success', `Successfully enrolled ${sids}.`);
-            res.status(200).redirect('back');
+            console.log('2b');
+            const update_sql = pgp.helpers.update(selected_rows, column_set) + where_sql;
+    
+            pool.query(update_sql, (err, data) => {
+                if (err) {
+                    res.status(err.status || 500);
+                    res.render('error', {
+                        message: "Something went wrong during the update, try again later.",
+                        error: err
+                    });
+                } else {
+                    req.flash('success', `Successfully enrolled ${sids}.`);
+                    res.status(200).redirect('back');
+                }
+            });
         }
-	});
+    });
 });
 
 // POST
