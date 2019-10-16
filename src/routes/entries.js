f68888a4d4bb499fef2e987742a676b0a2097594
jacqcjn <jacquelinecjn@gmail.com>
2019-10-16 16:18:40 +0800

    Update forum entries to show user's name

--- src/routes/entries.js
+++ src/routes/entries.js
@@ -14,9 +14,14 @@ router.get('/', function(req, res, next) {
 		return res.redirect('/login');
 	}
 	
-    var sql_query = `SELECT u_id, TO_CHAR(e_datetime, 'Dy Mon DD YYYY HH24:MI:SS') e_datetime, e_content
-    FROM ForumEntries WHERE c_id =\'${req.cid}\' and f_datetime =\'${req.f_datetime}\'`;
-	pool.query(sql_query, (err, entries) => {
+    var show_entries = 
+    'SELECT fe.u_id, u_name, TO_CHAR(e_datetime, \'Dy Mon DD YYYY HH24:MI:SS\') e_datetime, e_content'
+    + ' FROM ForumEntries fe JOIN Accounts a'
+    + ' ON fe.u_id = a.u_id'
+    + ' WHERE c_id = $1' 
+    + ' AND f_datetime = $2';
+
+	pool.query(show_entries, [req.cid, req.f_datetime], (err, entries) => {
 		res.render('entries', {
 			isCourse: req.isCourse,
 			username: req.user.u_name,
