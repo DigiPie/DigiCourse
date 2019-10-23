e05feb8bcff4fa14864b9f55ab6975b440614cfe
Bryan <bryankoh137@hotmail.com>
2019-10-24 03:49:19 +0800

    added searchbar to applicationStatus

--- src/routes/applicationStatus.js
+++ src/routes/applicationStatus.js
@@ -93,6 +93,61 @@ router.post('/', function(req, res, next) {
 				res.status(200).redirect('/applicationStatus');
 			}
 		});
+	} else if (req.body.searchBox) {
+		// Prepare SQL Statement
+		var sql_query;
+		if (req.user.u_type == 'Professor') {
+			sql_query = `
+				WITH CurrentSemCourses AS (
+					SELECT c_code, c_year, c_sem, c_capacity
+					FROM CourseYearSem NATURAL JOIN (
+						SELECT c_year, c_sem 
+						FROM CourseYearSem
+						GROUP BY c_year, c_sem
+						ORDER BY c_year DESC, c_sem DESC
+						LIMIT 1 
+					) AS yearsem
+				)
+				SELECT A.u_name AS studentname, S.s_id, CD.c_code, CD.c_name, TO_CHAR(E.req_datetime, 'Dy Mon DD YYYY HH24:MI:SS') AS req_datetime, E.req_type, E.req_status, E.p_id AS approver
+				FROM Enrollments E NATURAL JOIN CourseDetails CD NATURAL JOIN Students S JOIN Accounts A ON E.s_id = A.u_username
+				WHERE EXISTS (
+					SELECT 1 FROM CurrentSemCourses C NATURAL JOIN Manages M
+					WHERE E.c_code=C.c_code AND E.c_year=C.c_year AND E.c_sem=C.c_sem
+						AND p_id = $1
+				) AND LOWER(c_name) LIKE LOWER($2) OR LOWER(c_code) LIKE LOWER($2)
+				ORDER BY c_code, req_status, approver`;
+		} else {
+			sql_query = `
+				WITH CurrentSemCourses AS (
+					SELECT c_code, c_year, c_sem, c_capacity
+					FROM CourseYearSem NATURAL JOIN (
+						SELECT c_year, c_sem 
+						FROM CourseYearSem
+						GROUP BY c_year, c_sem
+						ORDER BY c_year DESC, c_sem DESC
+						LIMIT 1 
+					) AS yearsem
+				)
+				SELECT A.u_name AS studentname, S.s_id, CD.c_code, CD.c_name, TO_CHAR(E.req_datetime, 'Dy Mon DD YYYY HH24:MI:SS') AS req_datetime, E.req_type, E.req_status, E.p_id AS approver
+				FROM Enrollments E NATURAL JOIN CourseDetails CD NATURAL JOIN Students S JOIN Accounts A ON E.s_id = A.u_username
+				WHERE EXISTS (
+					SELECT 1 FROM CurrentSemCourses C
+					WHERE E.c_code=C.c_code AND E.c_year=C.c_year AND E.c_sem=C.c_sem
+				) AND S.s_id=$1 AND (LOWER(c_name) LIKE LOWER($2) OR LOWER(c_code) LIKE LOWER($2))
+				ORDER BY c_code, req_status, approver`;
+		}
+
+		// Query
+		pool.query(sql_query, [req.user.u_username, "%" + req.body.searchBox + "%"], (err, data) => {
+			res.render('applicationStatus', { 
+				isCourse: false, 
+				username: req.user.u_name,
+				accountType: req.user.u_type, 
+				uid: req.user.u_username,
+				datarows: data.rows,
+				dataNum: data.rowCount
+			});
+		});
 	} else {
 		return res.redirect('/course/' + cid);
 	}
