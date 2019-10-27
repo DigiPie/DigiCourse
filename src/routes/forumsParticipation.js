5cd3fda8a6544ac439a259a2ccccfb931aabc663
jacqcjn <jacquelinecjn@gmail.com>
2019-10-28 04:08:14 +0800

    Split forum participation stats for students and teaching assistants

--- src/routes/forumsParticipation.js
+++ src/routes/forumsParticipation.js
@@ -14,8 +14,8 @@ router.get('/', function(req, res, next) {
 		return res.redirect('/login');
     }
 
-    // Retrieve a list of students who holds the rank of the top 5 forum participation.
-    var get_top_participants =
+    // Retrieve a list of students (not TA) who holds the rank of the top 5 forum participation.
+    var get_top_student_participants =
 	'WITH entriesPerForum AS '
     + ' ( SELECT u_username, u_name, f_topic, COUNT(u_username) as num_of_entries'
     + '   FROM Accounts NATURAL JOIN ForumEntries fe NATURAL JOIN Forums f'
@@ -26,6 +26,62 @@ router.get('/', function(req, res, next) {
     + '   ( SELECT 1'
     + '     FROM Professors'
     + '     WHERE u_username = p_id'
+    + '   )'
+    + '   AND NOT EXISTS'
+    + '   ( SELECT 1'
+    + '     FROM Enrollments e'
+    + '     WHERE e.s_id = u_username'
+	+ '     AND e.c_code = $1'
+    + '     AND e.c_year = $2'
+    + '     AND e.c_sem = $3'
+	+ '     AND e.req_type = 0'
+    + '     AND e.req_status = \'TRUE\''
+    + '   )'
+    + '   GROUP BY u_username, f_topic'
+    + '   ORDER BY num_of_entries desc'
+    + ' )' 
+    
+    + ' SELECT epf.u_username, epf.u_name, te.total_entries, epf.f_topic as most_Participated_forum, epf.num_of_entries'
+    + ' from entriesPerForum epf LEFT JOIN '
+    + ' ( SELECT u_username, COUNT(u_username) as total_entries, rank() over (order by COUNT(u_username) desc)'
+    + '   FROM ForumEntries'
+    + '   WHERE c_code = $1'
+    + '   AND c_year = $2'
+    + '   AND c_sem = $3'
+    + '   GROUP BY u_username'
+    + ' ) te'
+    + ' ON epf.u_username = te.u_username'
+    + ' WHERE NOT EXISTS'
+    + ' ( SELECT *'
+    + '   FROM entriesPerForum epf2'
+    + '   WHERE epf.u_username = epf2.u_username'
+    + '   AND epf.num_of_entries < epf2.num_of_entries'
+    + ' )'
+    + ' AND te.rank <= 5'
+    + ' ORDER BY te.total_entries desc';
+
+    // Retrieve a list of teaching assistants who holds the rank of the top 5 forum participation.
+    var get_top_teaching_participants =
+	'WITH entriesPerForum AS '
+    + ' ( SELECT u_username, u_name, f_topic, COUNT(u_username) as num_of_entries'
+    + '   FROM Accounts NATURAL JOIN ForumEntries fe NATURAL JOIN Forums f'
+    + '   WHERE c_code = $1'
+    + '   AND c_year = $2'
+    + '   AND c_sem = $3'
+    + '   AND NOT EXISTS'
+    + '   ( SELECT 1'
+    + '     FROM Professors'
+    + '     WHERE u_username = p_id'
+    + '   )'
+    + '   AND EXISTS'
+    + '   ( SELECT 1'
+    + '     FROM Enrollments e'
+    + '     WHERE e.s_id = u_username'
+	+ '     AND e.c_code = $1'
+    + '     AND e.c_year = $2'
+    + '     AND e.c_sem = $3'
+	+ '     AND e.req_type = 0'
+    + '     AND e.req_status = \'TRUE\''
     + '   )'
     + '   GROUP BY u_username, f_topic'
     + '   ORDER BY num_of_entries desc'
@@ -50,14 +106,17 @@ router.get('/', function(req, res, next) {
     + ' AND te.rank <= 5'
     + ' ORDER BY te.total_entries desc';
     
-	pool.query(get_top_participants, [req.cid, req.year, req.sem], (err, participation) => {
-        res.render('forumsParticipation', {
-            isCourse: req.isCourse,
-            username: req.user.u_name,
-            accountType: req.user.u_type, 
-            cid: req.cid,
-            data: req.data,
-            participation: participation.rows,
+	pool.query(get_top_student_participants, [req.cid, req.year, req.sem], (err, students) => {
+        pool.query(get_top_teaching_participants, [req.cid, req.year, req.sem], (err, teaching) => {
+            res.render('forumsParticipation', {
+                isCourse: req.isCourse,
+                username: req.user.u_name,
+                accountType: req.user.u_type, 
+                cid: req.cid,
+                data: req.data,
+                students: students.rows,
+                teaching: teaching.rows
+            });
         });
     });
 });
